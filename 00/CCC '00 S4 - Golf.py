c3c8a9a6411a6831c61a1815e307bb11d899a5ba
brokentelescope <72772851+brokentelescope@users.noreply.github.com>
2022-10-19 19:01:49 -0400

    Update CCC '00 S4 - Golf.py

--- 00/CCC '00 S4 - Golf.py
+++ 00/CCC '00 S4 - Golf.py
@@ -1,14 +1,4 @@
-# import time
-# start_time = time.time()
-
-
-#SORRY GUYS I MDESPERATE
-#SORRY GUYS I MDESPERATE
-#SORRY GUYS I MDESPERATE
-#SORRY GUYS I MDESPERATE
-#SORRY GUYS I MDESPERATE
-#SORRY GUYS I MDESPERATE
-#SORRY GUYS I MDESPERATE
+from collections import deque
 
 dist = int(input())
 hm_clubs = int(input())
@@ -17,46 +7,29 @@ clubs = []
 for x in range(hm_clubs):
     clubs.append(int(input()))
 
-queue = [0]
-counter = 0
-visited = []
 def golf(dist,clubs):
-    counter = 0
-    while len(queue) != 0:
+    
+    queue = []
+    queue.append(0)
+    steps = 0
+    visited = [False] * 5281
+    
+    while queue:
         for x in range(len(queue)):
             node = queue[0]
         
             if node == dist:
-                # return f"Roberta wins in {counter} strokes."
-                return counter
+                return f"Roberta wins in {steps} strokes."
 
             for x in clubs:
                 if node+x <= dist:
-                    if node+x not in visited:
+                    if not visited[node+x]:
                         queue.append(node+x)
-                        visited.append(node+x)
-
+                        visited[node+x] = True
             
             queue.pop(0)
-        counter += 1
+        steps += 1
 
-    return -1
-    # return "Roberta acknowledges defeat."
+    return "Roberta acknowledges defeat."
 
-if hm_clubs == 32 and dist == 5280:
-    loop = dist//max(clubs)
-    while loop != -1:
-        turns = golf(dist-max(clubs)*loop, clubs)
-        if turns != -1:
-            print(f"Roberta wins in {loop + turns} strokes.")
-            break
-        loop -= 1
-else:
-    strokes = golf(dist,clubs)
-    if strokes == -1:
-        print("Roberta acknowledges defeat.")
-    else:
-        print(f"Roberta wins in {strokes} strokes.")
-
-# print((golf(dist-max(clubs)*x,clubs))
-# print((time.time() - start_time))
+print(golf(dist, clubs))
