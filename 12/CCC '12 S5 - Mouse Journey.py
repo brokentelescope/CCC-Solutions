b8d0a79c986328404c388a036bdc0b5d1dc37963
Owen Chen <72772851+brokentelescope@users.noreply.github.com>
2022-12-05 21:24:17 -0500

    Update CCC '12 S5 - Mouse Journey.py

--- 12/CCC '12 S5 - Mouse Journey.py
+++ 12/CCC '12 S5 - Mouse Journey.py
@@ -23,71 +23,9 @@ for x in range(int(input())):
 
     board[cr-1][cc-1] = 0
 
-    # for x in board:
-    #     print(" ".join([str(y) for y in x]))
-    # print()
-
 for r in range(1,rows):
     for c in range(1,columns):
         if board[r][c] != 0:
             board[r][c] = board[r-1][c] + board[r][c-1]
-            # for x in board:
-            #     print(" ".join([str(y) for y in x]))
-            # print()
 
 print(board[-1][-1])
-# input
-#3 4
-
-#output if i use board1
-# 1 1 1 1
-# 1 2 -1 -1
-# 1 -1 -1 -1
-
-# 1 1 1 1
-# 1 2 3 -1
-# 1 -1 -1 -1
-
-# 1 1 1 1
-# 1 2 3 4
-# 1 -1 -1 -1
-
-# 1 1 1 1
-# 1 2 3 4
-# 1 3 -1 -1
-
-# 1 1 1 1
-# 1 2 3 4
-# 1 3 6 -1
-
-# 1 1 1 1
-# 1 2 3 4
-# 1 3 6 10
-
-# input
-# 3 4
-
-#output when i use board          
-# 1 1 1 1
-# 1 2 -1 -1
-# 1 2 -1 -1
-
-# 1 1 1 1
-# 1 2 3 -1
-# 1 2 3 -1
-
-# 1 1 1 1
-# 1 2 3 4
-# 1 2 3 4
-
-# 1 1 1 1
-# 1 3 3 4
-# 1 3 3 4
-
-# 1 1 1 1
-# 1 3 6 4
-# 1 3 6 4
-
-# 1 1 1 1
-# 1 3 6 10
-# 1 3 6 10
