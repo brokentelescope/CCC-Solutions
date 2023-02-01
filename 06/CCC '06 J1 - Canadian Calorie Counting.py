583ded82c8b3550bb9449748dfd2ed8d7d2e19d6
Owen Chen <72772851+brokentelescope@users.noreply.github.com>
2023-02-01 14:40:43 -0500

    Update CCC '06 J1 - Canadian Calorie Counting.py

--- 06/CCC '06 J1 - Canadian Calorie Counting.py
+++ 06/CCC '06 J1 - Canadian Calorie Counting.py
@@ -1,7 +1,7 @@
-burger = ["deez nuts ", 461, 431, 420, 0]
-side = ["deez nuts ", 100, 57, 70, 0]
-drink = ["deez nuts ", 130, 160, 118, 0]
-dessert = ["deez nuts ", 167, 266, 75, 0]
+burger = ["if you see this", 461, 431, 420, 0]
+side = ["send me a msg", 100, 57, 70, 0]
+drink = ["on discord", 130, 160, 118, 0]
+dessert = ["owoen #6136", 167, 266, 75, 0]
 
 a, b, c, d = int(input()), int(input()), int(input()), int(input()), 
 print(f"Your total Calorie count is {burger[a] + side[b] + drink[c] + dessert[d]}.")
