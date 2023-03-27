bcb6677d7e4743fa8f1fe15b8ccfffce7b53c08f
Owen Chen <72772851+brokentelescope@users.noreply.github.com>
2023-03-26 21:17:36 -0400

    Update CCC '23 S2 - Symmetric Mountains.cpp

--- 23/CCC '23 S2 - Symmetric Mountains.cpp
+++ 23/CCC '23 S2 - Symmetric Mountains.cpp
@@ -1,53 +1,32 @@
-#include <bits/stdc++.h>
+# interval dp
 
-using namespace std;
+n = int(input())
 
-int arr[5000];
+arr = [int(x) for x in input().split()] 
 
-int dp[5000][5000];
+dp = [[0] * n for x in range(n)]
 
-int ans[5001];
+dist = 1
 
-int n;
 
-const int inf = numeric_limits<int>::max();
+ans = [float('inf')] * n
+ans[0] = 0
 
-int solve(int l, int r) {
-    if (dp[l][r] != -1) return dp[l][r];
+for x in range(1, n+1):
+    a = 0
+    b = x 
 
-    if (r - l <= 0) return 0;
+    while b < n:
+        
+        if x == 1:
+            dp[a][b] = abs(arr[a]-arr[b])
+        else:
+            dp[a][b] = abs(arr[a] - arr[b]) + dp[a+1][b-1]
+        
+        ans[x] = min(ans[x], dp[a][b])
 
-    int result = abs(arr[l]-arr[r]) + solve(l+1, r-1);
-
-    dp[l][r] = result;
-    ans[r-l+1] = min(ans[r-l+1], result);
-    return result;
-}
-
-int main() {
-    cin.sync_with_stdio(0); cin.tie(0);    
-
-    cin >> n; 
-
-    for (int x = 0; x < n; x++) {
-        cin >> arr[x];
-        ans[x] = inf;
-    }
-    ans[n] = inf; 
-    for (int x = 0; x < n; x++) {
-        for (int y = 0; y < n; y++) {
-            dp[x][y] = -1;
-        }
-    }
-
-    for (int x = 0; x < n; x++) {
-        for (int y = 0; y < n; y++) {
-            solve(x, y);
-        }
-    }
-
-    cout << 0 << " ";
-    for (int x = 2; x <= n; x++) {
-        cout << ans[x] << " ";
-    }
-}
+        a += 1
+        b += 1
+        
+for x in range(n):
+    print(ans[x], end = " ")
